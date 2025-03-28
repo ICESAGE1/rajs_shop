37bd4df7f0bd7e2e0d21982d1d7f5e3e4a669e1e
ICESAGE1 <166036148+ICESAGE1@users.noreply.github.com>
2025-03-29 03:21:25 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import RajsShop from "./components/RajsShop";
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Raj's Shop</h1>
+      <RajsShop />
+    </div>
+  );
+}
+
+export default App;
