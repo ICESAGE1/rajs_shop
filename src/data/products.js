cdacc00cac795513bb3d1acf2ba75d145c80cb2b
ICESAGE1 <166036148+ICESAGE1@users.noreply.github.com>
2025-04-06 01:25:58 +0530

    Create products.js

---
+++ src/data/products.js
@@ -0,0 +1,34 @@
+const products = [
+  {
+    id: 1,
+    name: 'Laptop',
+    price: 50000,
+    image: '/images/laptop.png',
+  },
+  {
+    id: 2,
+    name: 'Smartphone',
+    price: 20000,
+    image: '/images/phone.png',
+  },
+  {
+    id: 3,
+    name: 'Headphones',
+    price: 3000,
+    image: '/images/headphones.png',
+  },
+  {
+    id: 4,
+    name: 'Video Editing',
+    price: 1500,
+    image: '/images/video_editing.png',
+  },
+  {
+    id: 5,
+    name: 'Poster Making',
+    price: 800,
+    image: '/images/poster_making.png',
+  },
+];
+
+export default products;
