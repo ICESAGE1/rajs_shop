6f9f676e61e3b70a2306deb656a90168dfada812
ICESAGE1 <166036148+ICESAGE1@users.noreply.github.com>
2025-04-06 01:31:09 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Product from './Product';
+import Products from './Products';
 import './App.css';
 
 const App = () => {
   return (
     <div className="app">
-      {/* Amazon-like header */}
+      {/* Amazon-style header */}
       <header className="header">
         <img
           className="logo"
@@ -20,13 +20,26 @@ const App = () => {
 
       {/* Product section */}
       <div className="productSection">
-        <Product
+        <Products
           id="1"
           title="Video Editing Service"
           price={499}
           image="https://cdn.pixabay.com/photo/2020/05/28/21/35/youtube-5230870_1280.jpg"
           rating={4}
         />
-        <Product
-          id
+        <Products
+          id="2"
+          title="Poster Making Service"
+          price={299}
+          image="https://cdn.pixabay.com/photo/2017/01/31/20/24/poster-2022853_1280.png"
+          rating={5}
+        />
+        {/* You can add more <Products /> here */}
+      </div>
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
 
